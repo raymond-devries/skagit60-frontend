b233aaa511b90ce407a5ea4150767bdd69bc2954
raymonddevries <raymond.l.devries@gmail.com>
2020-08-08 23:43:01 -0700

    Add vuex store.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import App from "@/App";
 import router from "@/router";
+import store from "@/store";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store: store,
   router,
   render: (h) => h(App),
 }).$mount("#app");
